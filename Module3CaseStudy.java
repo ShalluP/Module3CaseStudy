4fbf1dfa5b0d0a6f107ce80ade06cfbe7ccd9028
Shallu <shallupahwa@gmail.com>
2019-08-07 16:06:19 +0200

    Second Commit

--- Module3CaseStudy.java
+++ Module3CaseStudy.java
@@ -6,23 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-/*Write a selenium automation code to perform these steps: -
-??6. Set selenium script timeout and execute any asynchronous script
-(Ex: window.setTimeout(arguments[arguments. Length - 1], 500);) to
-validate the script timeout
-7. Search for “Selenium” course in the search box and press “Enter”
-8. Add explicit wait for page to navigate to selenium course and click on the
-course
-9. Wait for the whole selenium course page to be loaded and get the title of
-the page
-10. Validate the page title to be “Selenium 3.0 WebDriver Online Training”
-11. Navigate back to the previous page
-12. Wait for whole page to be loaded and now select “All courses” from the
-menu option
-13. Add a fluent wait script to wait for whole page to be loaded under “All
-course”
-*/
+
 public class Module3CaseStudy {
 
 	public static void main(String[] args) {
@@ -31,20 +19,55 @@ public class Module3CaseStudy {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		//driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS); //Setting the pageload timeout for 30 seconds
+		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS); //Setting the pageload timeout for 120 seconds
 		
 		driver.get("https://www.edureka.co/");
 		
-		//driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Setting the implicit wait for all the activities
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); // Setting the implicit wait for all the activities
+		
+		driver.manage().timeouts().setScriptTimeout(100, TimeUnit.SECONDS); // Setting the script time out
+		
+	//	driver.setTimeout(arguments[arguments. Length - 1], 500);
 		
 		// Search for Selenium course in the search bar on the edureka website
 		
 		WebElement searchBox = driver.findElement(By.id("search-inp"));
-		searchBox.sendKeys("Selenium Certification Training");
-		searchBox.click();
 		
+		searchBox.sendKeys("Selenium");
+		
+		driver.findElement(By.id("search-button-top")).click(); // clicking on the search icon after selecting the course.
+		
+	// explicit wait on the page 8. Add explicit wait for page to navigate to selenium course and click on the course
+		
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h3[text()='Selenium Certification Training ']"))).click();
+		
+		// get the title
+		
+		String title = driver.getTitle();
+		System.out.println("The title of the page is : "+title);
+		
+		boolean titleValidate = title.equalsIgnoreCase("Selenium 3.0 WebDriver Online Training");
+		
+		System.out.println(titleValidate);
+		
+		// navigate back
+		driver.navigate().back();
+		driver.navigate().back();
+		
+		driver.findElement(By.xpath("//a[text()='Courses']")).click(); // click on all-courses
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		// Adding fluent wait for the whole page to load
+		
+		FluentWait<WebDriver> fwait = new FluentWait<WebDriver>(driver);
+		fwait.pollingEvery(30, TimeUnit.SECONDS);
+		
+		driver.close();
 		
 	}
 
